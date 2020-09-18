8d4038fc7676b933714ad30519b85961beaafdf9
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 13:43:49 +0100

    Update nftables_apply.sh

--- scripts/nftables_apply.sh
+++ scripts/nftables_apply.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-### Add rules ###
+### Flusing ruleset and add missing route ###
+echo '[info] Flusing ruleset'
+nft flush ruleset
+add_route="$(ip route | grep 'default')" ; add_route="$(sed "s|default|$HOST_NETWORK|g" <<< $add_route)"
+ip route add $add_route
+echo "[info] Added route $add_route"
+
+### Apply rules ###
 echo '[info] Apply rules'
 nft -f /nftables.rules
 rm /nftables.rules
