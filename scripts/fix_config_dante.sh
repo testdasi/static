aaf58a427c4f4e5fb4c7f9e7b10cafe46d373f5b
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 20:09:26 +0100

    Update fix_config_dante.sh

--- scripts/fix_config_dante.sh
+++ scripts/fix_config_dante.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /etc/dante \
     && cp -n /static/config/danted.conf /etc/dante/
-sed -i "s|internal: eth0 port=1080|internal: eth0 port=$DANTE_PORT|g" '/config/dante/danted.conf'
+sed -i "s|internal: eth0 port=1080|internal: eth0 port=$DANTE_PORT|g" '/etc/dante/danted.conf'
 echo '[info] danted fixed.'
