5751ce1ef10a51f039cb7b3856bbafd9d336fcfe
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 20:14:30 +0100

    Update fix_config_deluge.sh

--- scripts/fix_config_deluge.sh
+++ scripts/fix_config_deluge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p /config/deluge-web \
-    && cp -n /static/config/deluge.conf /config/deluge-web/core.conf \
-    && cp -n /static/config/deluge-web.conf /config/deluge-web/web.conf \
+mkdir -p /etc/deluge-web \
+    && cp -n /static/config/deluge.conf /etc/deluge-web/core.conf \
+    && cp -n /static/config/deluge-web.conf /etc/deluge-web/web.conf \
     && mkdir -p /data/deluge/watch \
     && mkdir -p /data/deluge/incomplete \
     && mkdir -p /data/deluge/complete \
     && mkdir -p /data/deluge/torrent
-sed -i "s|\"port\": 8112|\"port\": $DELUGE_PORT|g" '/config/deluge-web/web.conf'
+sed -i "s|\"port\": 8112|\"port\": $DELUGE_PORT|g" '/etc/deluge-web/web.conf'
 echo '[info] deluge fixed.'
