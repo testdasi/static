f7a093b6fc5e0e31d8f5099dda0f72b9b9252434
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 20:11:05 +0100

    Update fix_config_nzbhydra2.sh

--- scripts/fix_config_nzbhydra2.sh
+++ scripts/fix_config_nzbhydra2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p /config/nzbhydra2 \
-    && cp -n /static/config/nzbhydra.yml /config/nzbhydra2/
-sed -i "s|port: 5076|port: $HYDRA_PORT|g" '/config/nzbhydra2/nzbhydra.yml'
-sed -i "s|127\.0\.0\.1:8080|127\.0\.0\.1:$SAB_PORT_A|g" '/config/nzbhydra2/nzbhydra.yml'
+mkdir -p /etc/nzbhydra2 \
+    && cp -n /static/config/nzbhydra.yml /etc/nzbhydra2/
+sed -i "s|port: 5076|port: $HYDRA_PORT|g" '/etc/nzbhydra2/nzbhydra.yml'
+sed -i "s|127\.0\.0\.1:8080|127\.0\.0\.1:$SAB_PORT_A|g" '/etc/nzbhydra2/nzbhydra.yml'
 echo '[info] nzbhydra2 fixed.'
