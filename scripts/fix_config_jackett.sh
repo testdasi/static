65e03c30b9b0158e5f6b3065b0ea7595af31fa82
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-03 21:54:00 +0100

    Update fix_config_jackett.sh

--- scripts/fix_config_jackett.sh
+++ scripts/fix_config_jackett.sh
@@ -3,4 +3,6 @@
 mkdir -p /etc/jackett \
     && cp -n /static/config/jackett.json /etc/jackett/ServerConfig.json
 sed -i "s|: 9117|: $JACKETT_PORT|g" '/etc/jackett/ServerConfig.json'
+sed -i "s|: 1080|: $DANTE_PORT|g" '/etc/jackett/ServerConfig.json'
+sed -i "s|localhost|$ETH0_IP|g" '/etc/jackett/ServerConfig.json'
 echo '[info] jackett fixed.'
