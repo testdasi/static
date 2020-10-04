85c0067a840b427361e84b483d560aeb3c4bbfd4
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-04 10:42:54 +0100

    Update fix_config_tinyproxy.sh

--- scripts/fix_config_tinyproxy.sh
+++ scripts/fix_config_tinyproxy.sh
@@ -3,5 +3,5 @@
 mkdir -p /etc/tinyproxy \
     && cp -n /static/config/tinyproxy.conf /etc/tinyproxy/
 sed -i "s|Port 8080|Port $TINYPROXY_PORT|g" '/etc/tinyproxy/tinyproxy.conf'
-sed -i "s|upstream socks5 localhost:1080|upstream socks5 $ETH0_IP:$DANTE_PORT|g" '/etc/tinyproxy/tinyproxy.conf'
+sed -i "s|upstream socks5 localhost:1080|upstream socks5 localhost:$DANTE_PORT|g" '/etc/tinyproxy/tinyproxy.conf'
 echo '[info] tinyproxy fixed.'
