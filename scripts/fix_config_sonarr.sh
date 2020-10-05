d18448a9c7e67f417e9e6bb3e8c0fea9ec1f05db
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-05 10:29:50 +0100

    Update fix_config_sonarr.sh

--- scripts/fix_config_sonarr.sh
+++ scripts/fix_config_sonarr.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 mkdir -p /etc/sonarr \
-    && cp -n /static/config/sonarr.xml /etc/sonarr/config.xml
+    && cp -n /static/config/sonarr.xml /etc/sonarr/config.xml \
+    && mkdir -p /data/sonarr/downloads \
+    && mkdir -p /data/sonarr/recycle \
+    && mkdir -p /data/sonarr/watch \
+    && mkdir -p /tv
 sed -i "s|8989|$SONARR_PORT|g" '/etc/sonarr/config.xml'
 echo '[info] sonarr fixed.'
