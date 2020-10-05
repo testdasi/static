3c2bc971da9304a259257d4faef9df7f2201f747
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-05 10:29:11 +0100

    Update fix_config_radarr.sh

--- scripts/fix_config_radarr.sh
+++ scripts/fix_config_radarr.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 mkdir -p /etc/radarr \
-    && cp -n /static/config/radarr.xml /etc/radarr/config.xml
+    && cp -n /static/config/radarr.xml /etc/radarr/config.xml \
+    && mkdir -p /data/radarr/downloads \
+    && mkdir -p /data/radarr/recycle \
+    && mkdir -p /data/radarr/watch \
+    && mkdir -p /movies
 sed -i "s|7878|$RADARR_PORT|g" '/etc/radarr/config.xml'
 echo '[info] radarr fixed.'
